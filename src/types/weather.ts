30c58815d219bc5c19ac99e85d03168817591022
Solest <zkordo044@gmail.com>
2025-03-29 10:53:04 -0400

    Type api responses to get started.

---
+++ src/types/weather.ts
@@ -0,0 +1,78 @@
+export interface WeatherData {
+    latitude: number;
+    longitude: number;
+    resolvedAddress: string;
+    address: string;
+    timezone: string;
+    tzoffset: number;
+    description: string;
+    days: DayWeather[];
+    currentConditions?: CurrentConditions;
+    alerts?: Alert[];
+  }
+  
+  export interface DayWeather {
+    datetime: string;
+    datetimeEpoch: number;
+    temp: number;
+    feelslike: number;
+    tempmax: number;
+    tempmin: number;
+    humidity: number;
+    precip: number;
+    precipprob: number;
+    preciptype: string[] | null;
+    windspeed: number;
+    winddir: number;
+    pressure: number;
+    cloudcover: number;
+    uvindex: number;
+    conditions: string;
+    icon: string;
+    source: string;
+    sunrise?: string;
+    sunset?: string;
+    moonphase?: number;
+    hours?: HourWeather[];
+  }
+  
+  export interface HourWeather {
+    datetime: string;
+    datetimeEpoch: number;
+    temp: number;
+    feelslike: number;
+    humidity: number;
+    precip: number;
+    precipprob: number;
+    preciptype: string[] | null;
+    windspeed: number;
+    winddir: number;
+    pressure: number;
+    cloudcover: number;
+    uvindex: number;
+    conditions: string;
+    icon: string;
+  }
+  
+  export interface CurrentConditions {
+    datetime: string;
+    datetimeEpoch: number;
+    temp: number;
+    feelslike: number;
+    humidity: number;
+    precip: number;
+    precipprob: number;
+    preciptype: string[] | null;
+    windspeed: number;
+    winddir: number;
+    pressure: number;
+    cloudcover: number;
+    uvindex: number;
+    conditions: string;
+    icon: string;
+  }
+  
+  export interface Alert {
+    event: string;
+    description: string;
+  }
