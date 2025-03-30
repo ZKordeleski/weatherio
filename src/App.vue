a08b1ef22d60ac85ec676e46093cfcf6e7d3e2f6
Solest <zkordo044@gmail.com>
2025-03-30 00:49:14 -0400

    Styles note.

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,7 @@ import WeatherComparison from './components/WeatherComparison.vue';
 </template>
 
 <style>
+/**TODO: Make styles file and generalize stuff for quick editing. */
 body {
   font-family: monospace, sans-serif;
   margin: 0;
